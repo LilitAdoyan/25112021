13d400ae991476eb1ba40024064ecabfc309a3ac
LilitAdoyan <lilith.adoyan@gmail.com>
2021-11-27 18:29:50 +0400

    25112021

--- My first app.js
+++ My first app.js
@@ -1,4 +1,13 @@
-const numberOfFilms=prompt("how many films did you watch?");
+let numberOfFilms;
+
+function start(){
+    numberOfFilms=+prompt("how many films did you watch?", "");
+    while(numberOfFilms==''||numberOfFilms==null||numberOfFilms==isNaN){
+        numberOfFilms=+prompt("how many films did you watch?", "");
+    }
+}
+
+start();
 const personalMovieDB={
     count: numberOfFilms,
     movies: {},
@@ -16,18 +25,23 @@ personalMovieDB.movies[c]=d;
 console.log(personalMovieDB);*/
 
 
-for (let i=0; i<2; i++){
-    const a = prompt("One of the movies you've watched recently", "");
-    const b = prompt("How do you rate it", "");
-if(a !=null&&b!=null&&a!=''&&b!=''&&a.length<50){
-    personalMovieDB.movies[a]=b;
-    console.log("done!");
-} else {
-    console.log("error!");
-    i--;
-}
-}
 
+function rememberMyFilms(){
+    for (let i=0; i<2; i++){
+        const a = prompt("One of the movies you've watched recently", "");
+        const b = prompt("How do you rate it", "");
+    if(a !=null && b!=null && a!='' &&b !='' && a.length<50){
+        personalMovieDB.movies[a]=b;
+        console.log("done!");
+    } else {
+        console.log("error!");
+        i--;
+    }
+    }
+}
+rememberMyFilms();
+
+function detectPersonalLevel(){
 if (personalMovieDB.count<10){
     console.log("Not so much!");
 } else if (personalMovieDB.count>=10&&personalMovieDB.count<30) {
@@ -37,3 +51,21 @@ if (personalMovieDB.count<10){
 } else {
     console.log("there is an error");
 }
+}
+detectPersonalLevel();
+function showMyDB(hidden){
+  if(!hidden){
+    console.log(personalMovieDB);
+  }
+}
+showMyDB(personalMovieDB.private);
+
+function writeYourGenres(){
+  for (let i=1; i<=3; i++){
+    let g=prompt(`your favorite ganre number ${i}`);
+
+    personalMovieDB.genres[i-1]=g;
+  }
+  
+}
+writeYourGenres();
